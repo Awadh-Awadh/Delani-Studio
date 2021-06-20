ddb4868996a048cc5e9b54fb53424aed00483dd4
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-20 10:04:58 +0300

    add functional on clicking events

---
+++ static/js/main.js
@@ -0,0 +1,30 @@
+$('.image1').click(()=>{
+  $('.description1').toggle();
+  $('.image1').toggle();
+  $('.head1').hide()
+})
+$('.description1').click(()=>{
+  $('.description1').toggle();
+  $('.image1').toggle();
+  $('.head1').show()
+})
+$('.image2').click(()=>{
+  $('.description2').toggle();
+  $('.image2').toggle();
+  $('.head2').hide()
+})
+$('.description2').click(()=>{
+  $('.description2').toggle();
+  $('.image2').toggle();
+  $('.head2').show()
+})
+$('.image3').click(()=>{
+  $('.description3').toggle();
+  $('.image3').toggle();
+  $('.head3').hide()
+})
+$('.description3').click(()=>{
+  $('.description3').toggle();
+  $('.image3').toggle();
+  $('.head3').show()
+})
