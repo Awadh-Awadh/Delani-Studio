4b2b0196c11ee1632983070772af3847753f4de0
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-06-20 12:38:28 +0300

    add hover effect

--- static/js/main.js
+++ static/js/main.js
@@ -30,7 +30,30 @@ $('.description3').click(()=>{
 })
 
 
-$()
+$('.works1').hover(()=>{
+  $('.work1').toggle()
+})
+$('.works2').hover(()=>{
+  $('.work2').toggle()
+})
+$('.works3').hover(()=>{  
+  $('.work3').toggle()
+})
+$('.works4').hover(()=>{
+  $('.work4').toggle()
+})
+$('.works5').hover(()=>{
+  $('.work5').toggle()
+})
+$('.works6').hover(()=>{
+  $('.work6').toggle()
+})
+$('.works7').hover(()=>{
+  $('.work7').toggle()
+})
+$('.works8').hover(()=>{
+  $('.work8').toggle()
+})
 
 
 
@@ -38,8 +61,8 @@ function validate(){
   let name = document.getElementById('fname').value;
   let mail = document.getElementById('mailing').value
   let message = document.getElementById('message').value
-  if(name.length <2){
-    alert('name is too short')
+  if(name.length <2 || name.length === ""){
+    alert('please Enter your full name')
   }
 
   
